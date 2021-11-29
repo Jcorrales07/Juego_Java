a8f4ea4fc9dc14deed4ea2c5bc441d3fb3f97e48
Joe Corrales <corralesjoe07@gmail.com>
2021-11-29 15:10:19 -0600

    creacion y diseño de los menus, creacion de la clase Main

---
+++ src/corrales_joe_juegojava/Main.java
@@ -0,0 +1,11 @@
+package corrales_joe_juegojava;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Main {
+    public static void main(String[] args) {
+        Menu.menuInicio();
+    }
+}
--- src/corrales_joe_juegojava/Menu.java
+++ src/corrales_joe_juegojava/Menu.java
@@ -1,12 +1,89 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+package corrales_joe_juegojava;
+
+import java.util.Scanner;
 
 /**
- *
  * @author Lenovo
  */
 public class Menu {
+    static Scanner input = new Scanner(System.in);
+
+    static byte opcion;
+    // Funcion que muestra el MENU DE INICIO
+    public static void menuInicio() {
+        System.out.println("\n\t     [== BIENVENIDO A GHOSTGAME ==]");
+        
+        do {
+            System.out.print("\n\t[=== MENU DE INICIO ===]"
+                + "\n  1) Log in"
+                + "\n  2) Registrar Jugador"
+                + "\n  0) Salir"
+                + "\nOPCION #");
+            opcion = input.nextByte(); 
+            
+            switch (opcion) {
+                case 1:
+                    
+                    // funcion de login
+                    /* Hacer la verificacion del usuario, si existe le das acceso
+                    al menu principal*/
+//                    System.out.println("Ha iniciado sesion correctamente!");
+                    menuPrincipal();
+                    // si algo esta mal entonces se le manda al menu principal
+                    // pero vamo a hacer que pregunte denuevo por que somos asi de pros
+                    break;
+                case 2:
+                    /* Primero se tiene que registrar, si se hace bien entonces se
+                    le da el menu principal. NO se pueden REPETIR los users 
+                    si algo esta mal entonces se le manda al menu principal
+                    pero vamo a hacer que pregunte denuevo por que somos asi de pros*/
+//                    System.out.println("Ha creado su cuenta correctamente!");
+                    menuPrincipal();
+                    break;
+                case 0:
+                    System.out.println("\nGracias por jugar!!"
+                            + "\nHA TERMINADO EL PROGRAMA...");
+                    break;
+                default:
+                    System.out.println("\nINGRESE UNA OPCION VALIDA!");
+            } 
+        } while (opcion != 0);
+    }
     
+    // Funcion que muestra el MENU PRINCIPAL
+    public static void menuPrincipal() {
+        System.out.println("\n\t\t»» HA INICIADO SESION! ««");
+        do {
+            System.out.print("\n\t[=== MENU PRINCIPAL ===]"
+                + "\n  1) Jugar Ghosts"
+                + "\n  2) Configuracion"
+                + "\n  3) Reportes"
+                + "\n  4) Mi Perfil"                    
+                + "\n  0) Cerrar Sesion"
+                + "\nOPCION #");
+                opcion = input.nextByte(); 
+        
+            switch (opcion) {
+                case 1:
+                    //JUGAR GHOSTS
+                    break;
+                case 2:
+                    //CONFIGURACION
+                    break;
+                case 3:
+                    //REPORTES
+                    break;
+                case 4:
+                    //MI PERFIL
+                    break;
+                case 0:
+                    System.out.println("\n\t\t»» HA CERRADO SESION! ««");
+                    menuInicio();
+                    break;
+                default:
+                    System.out.println("\nINGRESE UNA OPCION VALIDA!");       
+            }
+        } while (opcion != 0);
+        
+    }
 }
