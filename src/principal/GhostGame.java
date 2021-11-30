c258b5d3301333c95e75a4c168e095e33876fba1
Joe Corrales <corralesjoe07@gmail.com>
2021-11-30 16:36:19 -0600

    Primer intento de hacer la collection de players, fail #1

---
+++ src/principal/GhostGame.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package principal;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class GhostGame {
+    
+    public static void main(String[] args) {
+            Player mi = new Player("Jcorrales", "123456789");
+            mi.mostrarDatos();
+    }
+}
--- src/principal/Player.java
+++ src/principal/Player.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package principal;
 
 /**
@@ -10,5 +5,52 @@ package principal;
  * @author Lenovo
  */
 public class Player {
+
     
+   private String username;
+   private String password;
+   private double score;
+   
+   Player(String username, String password) {
+       this.username = username;
+       this.password = password;
+       score = 0;
+   }
+   
+   // SET y GET de username
+   public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    // SET y GET de password
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    // SET y GET de score
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+    
+    // Otras Funciones
+    public void mostrarDatos() {
+        System.out.println("╠╩═╩╬══════════════╬╩═╩╣");
+        System.out.println("╠═◘═╬╣ DATOS DEL JUGADOR ╠╬═◘═╣");
+        System.out.println("╠╦═╦╬══════════════╬╦═╦╣\n");
+        System.out.println("Usuario: "+ username
+            + "\nPuntaje: "+ score);
+        
+    }
 }
